f280416e9ef4bfb34936ea5fcba7f362030cd4ad
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-12-17 21:44:42 +0300

    Добавьте файлы проекта.

---
+++ Task14.3.3/Contact.cs
@@ -0,0 +1,18 @@
+﻿namespace Task14_3_3
+{
+    public class Contact
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
---
+++ Task14.3.3/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Task14_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            // сортировка телефонной книге
+            var sortPhoneBook = phoneBook.OrderBy(c => c.Name).ThenBy(c => c.LastName);
+
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
+            
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("--------------------------------------------------------------------");
+
+                var page = key.KeyChar;
+                switch (key.KeyChar)
+                {
+                    case '1':
+                        ConsoleView(sortPhoneBook.Take(2).ToList());
+                        break;
+                    case '2':
+                        ConsoleView(sortPhoneBook.Skip(2).Take(2).ToList());
+                        break;
+                    case '3':
+                        ConsoleView(sortPhoneBook.Skip(4).Take(2).ToList());
+                        break;
+                    default:
+                        ConsoleView(sortPhoneBook.Take(2).ToList());
+                        page = '1';
+                        break;
+                }
+
+                Console.WriteLine("--------------------------------------------------------------------");
+                Console.WriteLine("Страница " + page);
+
+                Console.WriteLine("\nВведите номер страницы: ");
+                key = Console.ReadKey();
+            }
+        }
+
+        // вывод телефонной книги в консоль
+        public static void ConsoleView(List<Contact> contacts)
+        {
+            foreach (Contact contact in contacts)
+            {
+                Console.WriteLine(" " + contact.Name + " " + contact.LastName + " - телефон " + contact.PhoneNumber + ", email " + contact.Email);
+            }
+        }
+    }
+}
